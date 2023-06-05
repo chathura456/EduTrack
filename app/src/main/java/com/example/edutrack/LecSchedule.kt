ad831b1e78005f1b8dd3df19b3ae48db2ce568e7
stephanfdo <k.stephanfdo@gmail.com>
2023-06-06 00:02:54 +0530

    few changes

--- app/src/main/java/com/example/edutrack/LecSchedule.kt
+++ app/src/main/java/com/example/edutrack/LecSchedule.kt
@@ -3,11 +3,12 @@ package com.example.edutrack
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 
-class LecWeicome : AppCompatActivity() {
+class LecSchedule : AppCompatActivity() {
 
     private lateinit var username: TextView
     private lateinit var modulebtn: LinearLayout
@@ -19,7 +20,7 @@ class LecWeicome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.lecture_welcome)
+        setContentView(R.layout.activity_schedule)
 
 
 
@@ -74,13 +75,70 @@ class LecWeicome : AppCompatActivity() {
 
 
 
+        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
 
+        // Time slots
+        val timeSlots = arrayOf(
+            "8:00 AM -\n9:00 AM\n", "9:00 AM -\n10:00 AM\n", "10:00 AM -\n11:00 AM\n",
+            "11:00 AM -\n12:00 PM\n", "12:00 PM -\n1:00 PM\n", "1:00 PM -\n2:00 PM\n"
+        )
 
+        // Subjects
+        val subjects = listOf("MAD", "Maths", "IAS", "Crypto", "Java", "SQA", "ITPM")
 
+        // Add table header
+        val headerRow = TableRow(this)
+        headerRow.addView(createTextView("Time", true))
 
+        for (i in 0 until 5) {
+            val dayTextView = createTextView(getDayName(i), true)
+            headerRow.addView(dayTextView)
+        }
+        tableLayout.addView(headerRow)
 
+        // Add time slots and randomly assign subjects to each day
+        for (timeSlot in timeSlots) {
+            val row = TableRow(this)
+            val timeTextView = createTextView(timeSlot, true)
+            row.addView(timeTextView)
+
+            val subjectsForDay = subjects.shuffled().take(5)
+            for (subject in subjectsForDay) {
+                val subjectTextView = createTextView(subject, false)
+                row.addView(subjectTextView)
+            }
+
+            tableLayout.addView(row)
+
+        }
+    }
+
+    private fun createTextView(text: String, isHeader: Boolean): TextView {
+        val textView = TextView(this)
+        textView.text = text
+        textView.gravity = Gravity.CENTER
+        if (isHeader) {
+            textView.setTypeface(null, android.graphics.Typeface.BOLD)
+        }
+        return textView
+    }
+
+    private fun getDayName(dayOfWeek: Int): String {
+        return when (dayOfWeek) {
+            0 -> "Monday\n"
+            1 -> "Tuesday\n"
+            2 -> "Wednesday\n"
+            3 -> "Thursday\n"
+            4 -> "Friday\n"
+            else -> ""
+        }
+    }
 
 
     }
 
-}
+
+
+
+
+
